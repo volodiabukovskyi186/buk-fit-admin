03682ad67d1f78ff6ebf17904823c3bbf0f8a228
Volodia Bukovskyi <volodiabukovskyi@gmail.com>
2025-09-23 18:07:54 +0300

    env: new prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyCMjsYrMGU8oPMKUnAeSKBt4NhHBoILc9s",
-    authDomain: "buk-panel.firebaseapp.com",
-    projectId: "buk-panel",
-    storageBucket: "buk-panel.firebasestorage.app",
-    messagingSenderId: "242660819108",
-    appId: "1:242660819108:web:644af883eb65474a772631"
+    apiKey: "AIzaSyAQZXct5nEyq9mxjUT_3-haD8kbKuHIn98",
+    authDomain: "buk-fit.firebaseapp.com",
+    projectId: "buk-fit",
+    storageBucket: "buk-fit.firebasestorage.app",
+    messagingSenderId: "479742325628",
+    appId: "1:479742325628:web:395cb9fcc11896040ceaf9",
+    measurementId: "G-LG246H4RLE"
   },
-
   botToken: '8033627783:AAFoa_Pzye9EcwK5_JbaCAuAbZbfGG8021U',
   miniAppLink: 'https://buk-fit-client-8b56ce9cc329.herokuapp.com/',
   botNewRegister: '8111781384:AAF5--svXCOVJwor5gaWDTtPx8F3Oxi4vtI',
