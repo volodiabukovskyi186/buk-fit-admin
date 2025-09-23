c67e476566685d473aabccb14e5d63b54422be2c
Volodia Bukovskyi <volodiabukovskyi@gmail.com>
2025-09-23 15:31:32 +0300

    first commit 1

---
+++ src/app/main/pages/meals-names/meals-names.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TableGridDataTypeEnum } from "../../../core/components/table-grid";
+import { MatDialog } from "@angular/material/dialog";
+import { UsersService } from "../users/users.service";
+import { Firestore, collection, getDocs, query, where, deleteDoc, doc, orderBy, limit, startAfter, DocumentSnapshot } from '@angular/fire/firestore';
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { forkJoin, switchMap, from } from "rxjs";
+
+@Component({
+  selector: 'app-meals-names',
+  templateUrl: './meals-names.component.html',
+  styleUrls: ['./meals-names.component.scss']
+})
+export class MealsNamesComponent implements OnInit {
+  tableGridDataTypeEnum = TableGridDataTypeEnum;
+  @ViewChild(MatPaginator) paginator: MatPaginator; // Додаємо пагінатор
+
+  users = [];
+  totalUsersCount = 0;
+  pageSize = 10; // Початковий розмір сторінки
+  lastVisible: DocumentSnapshot | null = null; // Для пагінації
+
+  constructor(
+    private dialog: MatDialog,
+    private usersService: UsersService,
+    private firestore: Firestore,
+    public snackBar: MatSnackBar,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getTotalUsersCount();
+    this.getUsers();
+  }
+
+  getTotalUsersCount() {
+    const exercisesCollection = collection(this.firestore, 'meals-names');
+    getDocs(exercisesCollection).then(snapshot => {
+      this.totalUsersCount = snapshot.size;
+    }).catch(error => console.error("Помилка отримання кількості страв: ", error));
+  }
+
+  getUsers(pageIndex: number = 0, newPageSize: number = this.pageSize): void {
+    this.pageSize = newPageSize; // ✅ Оновлюємо `pageSize`
+
+    let exercisesCollection = collection(this.firestore, 'meals-names');
+    let q = query(
+      exercisesCollection,
+      limit(this.pageSize)
+    );
+
+    if (this.lastVisible && pageIndex > 0) {
+      q = query(q, startAfter(this.lastVisible)); // Завантажуємо наступну сторінку
+    } else {
+      this.lastVisible = null; // ✅ Скидаємо `lastVisible`, якщо змінюється `pageSize`
+    }
+
+    getDocs(q).then(snapshot => {
+      if (!snapshot.empty) {
+        this.lastVisible = snapshot.docs[snapshot.docs.length - 1];
+        this.users = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        console.log('1111, this.users', this.users)
+      }
+    }).catch(error => console.error("Помилка отримання страв: ", error));
+  }
+
+  onPageChange(event: PageEvent) {
+    console.log("📌 Зміна сторінки:", event);
+
+    // ✅ Передаємо новий `pageSize`, якщо він змінюється
+    this.getUsers(event.pageIndex, event.pageSize);
+  }
+
+  deleteExercise(exercise) {
+    const collectionRef = collection(this.firestore, 'meals-names');
+    const q = query(collectionRef, where('id', '==', exercise.id));
+
+    getDocs(q).then(snapshot => {
+      if (!snapshot.empty) {
+        snapshot.forEach(docSnapshot => {
+          const docRef = doc(this.firestore, 'meals-names', docSnapshot.id);
+          deleteDoc(docRef).then(() => {
+            this.snackBar.open('страву успішно видалено', 'Закрити', { duration: 2000 });
+            this.getUsers(); // Оновлюємо список після видалення
+          }).catch(error => console.error("Помилка видалення страви:", error));
+        });
+      }
+    }).catch(error => console.error("Помилка пошуку страви:", error));
+  }
+
+  moveToExegesis(id) {
+    this.router.navigate([`/meals/edit-meals`, id]);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+//
+// export const environment = {
+//   firebase: {
+//     apiKey: "AIzaSyAQZ0S4oq7Wgs_ltAdWFwKLNb09a1IFSes",
+//     authDomain: "buk-panel-sbx.firebaseapp.com",
+//     projectId: "buk-panel-sbx",
+//     storageBucket: "buk-panel-sbx.firebasestorage.app",
+//     messagingSenderId: "916518342767",
+//     appId: "1:916518342767:web:25558a03ff312141017d9a"
+//   },
+//   botToken: '8033627783:AAFoa_Pzye9EcwK5_JbaCAuAbZbfGG8021U',
+//   // miniAppLink: 'https://buk-fit-client-8b56ce9cc329.herokuapp.com/',
+//   miniAppLink: 'https://41f1-194-44-253-73.ngrok-free.app',
+//   botNewRegister: '8111781384:AAF5--svXCOVJwor5gaWDTtPx8F3Oxi4vtI',
+//   welcomeMessageChannelID: -1002658415091,
+//
+//   };
+
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAQZXct5nEyq9mxjUT_3-haD8kbKuHIn98",
+    authDomain: "buk-fit.firebaseapp.com",
+    projectId: "buk-fit",
+    storageBucket: "buk-fit.firebasestorage.app",
+    messagingSenderId: "479742325628",
+    appId: "1:479742325628:web:395cb9fcc11896040ceaf9",
+    measurementId: "G-LG246H4RLE"
+  },
+  botToken: '8033627783:AAFoa_Pzye9EcwK5_JbaCAuAbZbfGG8021U',
+  miniAppLink: 'https://buk-fit-client-8b56ce9cc329.herokuapp.com/',
+  botNewRegister: '8111781384:AAF5--svXCOVJwor5gaWDTtPx8F3Oxi4vtI',
+  welcomeMessageChannelID: -1002658415091,
+};
+
